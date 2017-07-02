d640a3bd3db0c9a5c21740e9b2314b864ee63696
edfordshire <ed.lowther@gmail.com>
2017-07-02 09:45:24 +0100

    Docstring tweak

--- __main__.py
+++ __main__.py
@@ -3,6 +3,8 @@ from owlstone_test.json_exporter import json_exporter
 from owlstone_test.simple_server import run
 
 def main():
+    """Reads text files and performs textual analysis on them, before exporting relevant data as json 
+    and starting the server."""
     file_reader = Text_Processor()
     publishers = file_reader.read_files()
     json_exporter(publishers)
